a4f63d50f9b0021dff802f43f7ba24cdfdbb56b3
yebology <139052533+yebology@users.noreply.github.com>
2024-05-15 18:39:19 +0700

    add target

---
+++ MAC_ALP_MAD/MAC_ALP_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAC_ALP_MADApp.swift
+//  MAC_ALP_MAD
+//
+//  Created by MacBook Pro on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MAC_ALP_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
