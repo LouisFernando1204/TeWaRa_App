6d326d60b256ed52e419e5ce708316c986c1131f
Yobel <yobelnathaniel12@gmail.com>
2024-05-15 17:58:42 +0700

    folder revision

--- ALP_MAD/ALP_MADApp.swift
+++ ALP_MAD/ALP_MADApp.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 @main
 struct ALP_MADApp: App {
+    
     @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
-            IslandView()
-                .environment(modelData)
+            ContentView()
         }
     }
 }
