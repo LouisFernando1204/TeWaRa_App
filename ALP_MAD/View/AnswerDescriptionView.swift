f5110f1bfb9a9189d882fa8b574cd5bd55a40c47
yebology <139052533+yebology@users.noreply.github.com>
2024-05-31 12:36:29 +0700

    test1

--- ALP_MAD/View/AnswerDescriptionView.swift
+++ ALP_MAD/View/AnswerDescriptionView.swift
@@ -6,10 +6,230 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct AnswerDescriptionView: View {
+    
+    @StateObject private var islandController = IslandController(island: ModelData.shared.currentIslandObject)
+    @State private var navToIslandView = false
+    @State private var traditionalDanceDescription: TraditionalDance? = nil
+    @State private var traditionalLanguageDescription: TraditionalLanguage? = nil
+    
+    private var screenSize = ScreenSize()
+    
+    struct RoundedCorner: Shape {
+        var cornerRadius: CGFloat
+        var corners: UIRectCorner
+        
+        func path(in rect: CGRect) -> Path {
+            let path = UIBezierPath(
+                roundedRect: rect,
+                byRoundingCorners: corners,
+                cornerRadii: CGSize(width: cornerRadius, height: cornerRadius)
+            )
+            return Path(path.cgPath)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        let isIpad = self.screenSize.screenWidth > 600
+        
+        ScrollView {
+            VStack {
+                if let danceDescription = traditionalDanceDescription, let languageDescription = traditionalLanguageDescription {
+                    self.setUpAnswerDescriptionView(islandController: islandController, navToIslandView: $navToIslandView, traditionalDanceDescription: danceDescription, traditionalLanguageDescription: languageDescription, isIpad: isIpad)
+                }
+            }
+            .onAppear {
+                self.traditionalDanceDescription = self.islandController.showTraditionalDanceDescription()
+                self.traditionalLanguageDescription = self.islandController.showTraditionalLanguageDescription()
+            }
+        }
+        .safeAreaInset(edge: .top) {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: Color("redColor(TeWaRa)"), location: 0.5),
+                    .init(color: Color("orangeColor(TeWaRa)"), location: 1.0),
+                ]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .frame(height: isIpad ? self.screenSize.screenHeight/35 : self.screenSize.screenHeight/12)
+            .edgesIgnoringSafeArea(.top)
+            .padding(.bottom, isIpad ? -40 : -70)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
+                MusicPlayer.shared.startBackgroundMusic(musicTitle: "quizMusic", volume: 1)
+            }
+        }
+        .onDisappear {
+            MusicPlayer.shared.stopBackgroundMusic()
+        }
+        .onChange(of: self.navToIslandView) { oldValue, newValue in
+            if newValue {
+                MusicPlayer.shared.stopBackgroundMusic()
+            } else {
+                MusicPlayer.shared.startBackgroundMusic(musicTitle: "quizMusic", volume: 1)
+            }
+        }
+    }
+    
+    private func setUpAnswerDescriptionView(islandController: IslandController, navToIslandView: Binding<Bool>, traditionalDanceDescription: TraditionalDance, traditionalLanguageDescription: TraditionalLanguage, isIpad: Bool) -> some View {
+        VStack {
+            self.navigationBar(navToIslandView: navToIslandView, isIpad: isIpad)
+            if ModelData.shared.currentGame == "TraditionalDance" {
+                if let videoURL = Bundle.main.url(forResource: traditionalDanceDescription.image, withExtension: "mp4") {
+                    VideoPlayer(player: AVPlayer(url: videoURL))
+                        .frame(height: isIpad ? self.screenSize.screenHeight/2.555 : self.screenSize.screenHeight/3.85)
+                        .clipShape(
+                            RoundedCorner(cornerRadius: 40, corners: [.bottomLeft, .bottomRight])
+                        )
+                        .shadow(radius: 10, y: 4)
+                        .padding(.top, -8)
+                        .padding(.bottom, isIpad ? self.screenSize.screenHeight/40 : self.screenSize.screenHeight/80)
+                }
+                self.description(description: traditionalDanceDescription.description, answer: traditionalDanceDescription.answer, provinceOrigin: traditionalDanceDescription.provinceOrigin, title: "TARI", isIpad: isIpad)
+            } else {
+                if let image = traditionalLanguageDescription.image {
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: isIpad ? self.screenSize.screenHeight/2.2 : self.screenSize.screenHeight/4.5)
+                        .clipShape(
+                            RoundedCorner(cornerRadius: 40, corners: [.bottomLeft, .bottomRight])
+                        )
+                        .shadow(radius: 10, y: 4)
+                        .padding(.top, -8)
+                        .padding(.bottom, isIpad ? self.screenSize.screenHeight/40 : self.screenSize.screenHeight/80)
+                }
+                description(description: traditionalLanguageDescription.description, answer: traditionalLanguageDescription.answer, provinceOrigin: traditionalLanguageDescription.provinceOrigin, title: "BAHASA", isIpad: isIpad)
+            }
+            self.poinStatus(isIpad: isIpad)
+        }
+    }
+    
+    private func navigationBar(navToIslandView: Binding<Bool>, isIpad: Bool) -> some View {
+        HStack(alignment: .center) {
+            Text("                             ")
+            Spacer()
+            if isIpad {
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+            }
+            Text("Penjelasan")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .font(isIpad ? .title2 : .headline)
+            Spacer()
+            Spacer()
+            if isIpad {
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+            }
+            Button(
+                action: {
+                    navToIslandView.wrappedValue = true
+                },
+                label: {
+                    Text("Selesai")
+                        .font(isIpad ? .title2 : .headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                }
+            )
+            .cornerRadius(10)
+            .padding(.leading, isIpad ? self.screenSize.screenWidth/7.5 : self.screenSize.screenWidth/20)
+            .fullScreenCover(isPresented: navToIslandView, content: {
+                IslandView()
+            })
+            Spacer()
+        }
+        .padding(.bottom, isIpad ? self.screenSize.screenHeight/100 : 0)
+        .frame(height: isIpad ? self.screenSize.screenHeight/30 : self.screenSize.screenHeight/20)
+        .background(
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: Color("redColor(TeWaRa)"), location: 0.5),
+                    .init(color: Color("orangeColor(TeWaRa)"), location: 1.0)
+                ]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+        )
+    }
+    
+    private func description(description: String, answer: String, provinceOrigin: String, title: String, isIpad: Bool) -> some View {
+        VStack {
+            Rectangle()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(stops: [
+                            .init(color: Color("redColor(TeWaRa)"), location: 0.7),
+                            .init(color: Color("darkredColor(TeWaRa)"), location: 1.0)
+                        ]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .frame(width: isIpad ? self.screenSize.screenWidth/2.5 : self.screenSize.screenWidth/1.4, height: isIpad ? self.screenSize.screenHeight/17 : self.screenSize.screenHeight/12)
+                .padding(.top, -8)
+                .cornerRadius(32)
+                .overlay(
+                    Text("\(title) \(answer)")
+                        .font(.system(size: isIpad ? 36 : 32, weight: .bold))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                )
+                .padding(.bottom, isIpad ? self.screenSize.screenHeight/80 : self.screenSize.screenHeight/80)
+            Text("Asal: \(provinceOrigin)")
+                .font(.system(size: isIpad ? 20 : 16, weight: .bold))
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .italic()
+                .padding(.bottom, isIpad ? self.screenSize.screenHeight/40 : self.screenSize.screenHeight/65)
+            Text(description)
+                .font(.system(size: isIpad ? 24 : 20, weight: .regular))
+                .foregroundColor(.black)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal, isIpad ? self.screenSize.screenWidth/25 : self.screenSize.screenWidth/15)
+                .padding(.bottom, isIpad ? self.screenSize.screenHeight/30 : self.screenSize.screenHeight/80)
+                .lineSpacing(10)
+        }
+    }
+    
+    private func poinStatus(isIpad: Bool) -> some View {
+        Rectangle()
+            .fill(
+                LinearGradient(
+                    gradient: Gradient(stops: [
+                        .init(color: Color("redColor(TeWaRa)"), location: 0.5),
+                        .init(color: Color("orangeColor(TeWaRa)"), location: 1.0),
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+            )
+            .frame(width: isIpad ? self.screenSize.screenWidth/1.0855 : self.screenSize.screenWidth/1.16, height: isIpad ? self.screenSize.screenHeight/10 : self.screenSize.screenHeight/8.5)
+            .cornerRadius(10)
+            .overlay(
+                HStack {
+                    Spacer()
+                    Text("Poin mu sekarang adalah \(ModelData.shared.currentUser.score)")
+                        .font(.system(size: isIpad ? 25 : 20, weight: .regular))
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+            )
     }
 }
 
---
+++ ALP_MAD/View/SplashScreenView.swift
@@ -0,0 +1,172 @@
+//
+//  SplashScreenView.swift
+//  ALP_MAD
+//
+//  Created by Louis Fernando on 17/05/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    
+    @State private var isVisible = false
+    
+    var body: some View {
+        self.setUpSplashScreenView(isVisible: isVisible)
+    }
+    
+    private func setUpSplashScreenView(isVisible: Bool) -> some View {
+        ZStack{
+            // Warna background
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            
+            // Gradient-Wave pojok kiri atas
+            self.topLeadingGradientWave(isVisible: isVisible)
+            
+            // Gradient-Wave pojok kanan bawah
+            self.bottomTrailingGradientWave(isVisible: isVisible)
+            
+            // Logo
+            self.logo(isVisible: isVisible)
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 1.0)) {
+                self.isVisible = true
+            }
+        }
+    }
+    
+    private func topLeadingGradientWave(isVisible: Bool) -> some View {
+        VStack {
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                HStack {
+                    Image("gradientWave(TeWaRa)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 786.01, height: 380.76)
+                        .rotationEffect(.degrees(145.74))
+                        .offset(x: 0, y: isVisible ? -170 : -250)
+                    Spacer()
+                }
+                Spacer()
+            }
+            else if UIDevice.current.userInterfaceIdiom == .pad {
+                HStack {
+                    Image("gradientWave(TeWaRa)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 1200, height: 700)
+                        .rotationEffect(.degrees(145.74))
+                        .offset(x: -150, y: isVisible ? -200 : -250)
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .opacity(isVisible ? 1 : 0)
+    }
+    
+    private func bottomTrailingGradientWave(isVisible: Bool) -> some View {
+        VStack {
+            Spacer()
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                HStack {
+                    Spacer()
+                    Image("gradientWave(TeWaRa)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 706, height: 342)
+                        .rotationEffect(.degrees(-42.94))
+                        .offset(x: 10, y: isVisible ? 90 : 150)
+                }
+            }
+            else if UIDevice.current.userInterfaceIdiom == .pad {
+                HStack {
+                    Spacer()
+                    Image("gradientWave(TeWaRa)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 1200, height: 700)
+                        .rotationEffect(.degrees(-42.94))
+                        .offset(x: 150, y: isVisible ? 200 : 250)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .opacity(isVisible ? 1 : 0)
+    }
+    
+    private func logo(isVisible: Bool) -> some View {
+        VStack {
+            Spacer()
+            Spacer()
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                Image("logo(TeWaRa)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: isVisible ? 235 : 150, height: isVisible ? 214 : 150)
+            }
+            else if UIDevice.current.userInterfaceIdiom == .pad {
+                Image("logo(TeWaRa)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: isVisible ? 300 : 150, height: isVisible ? 250 : 150)
+            }
+            Spacer()
+            Text("TEBAK WAWASAN")
+                .font(.system(size: UIDevice.current.userInterfaceIdiom == .phone ? 20 : 30, weight: .bold))
+                .multilineTextAlignment(.center)
+                .tracking(4)
+                .foregroundColor(.clear)
+                .overlay(
+                    LinearGradient(
+                        gradient: Gradient(stops: [
+                            .init(color: Color("redColor(TeWaRa)"), location: 0.5),
+                            .init(color: Color("darkredColor(TeWaRa)"), location: 1.0),
+                        ]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                    .mask(
+                        Text("TEBAK WAWASAN")
+                            .font(.system(size: UIDevice.current.userInterfaceIdiom == .phone ? 20 : 30, weight: .bold))
+                            .tracking(4)
+                            .multilineTextAlignment(.center)
+                            .opacity(isVisible ? 1.0 : 0)
+                    )
+                )
+            Text("NUSANTARA")
+                .font(.system(size: UIDevice.current.userInterfaceIdiom == .phone ? 20 : 30, weight: .bold))
+                .multilineTextAlignment(.center)
+                .tracking(4)
+                .foregroundColor(.clear)
+                .overlay(
+                    LinearGradient(
+                        gradient: Gradient(stops: [
+                            .init(color: Color("redColor(TeWaRa)"), location: 0.5),
+                            .init(color: Color("darkredColor(TeWaRa)"), location: 1.0),
+                        ]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                    .mask(
+                        Text("NUSANTARA")
+                            .font(.system(size: UIDevice.current.userInterfaceIdiom == .phone ? 20 : 30, weight: .bold))
+                            .tracking(4)
+                            .multilineTextAlignment(.center)
+                            .opacity(isVisible ? 1.0 : 0)
+                    )
+                )
+                .padding(.top, -10)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .opacity(isVisible ? 1 : 0)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
