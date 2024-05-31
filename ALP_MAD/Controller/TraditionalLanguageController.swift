8031ed64c9c761c50c6168f3b6a2a650fe07438a
yebology <139052533+yebology@users.noreply.github.com>
2024-06-01 00:33:15 +0700

    done logic if correct and wrong answer traditional language

--- ALP_MAD/Controller/TraditionalLanguageController.swift
+++ ALP_MAD/Controller/TraditionalLanguageController.swift
@@ -18,10 +18,14 @@ class TraditionalLanguageController : ObservableObject {
         clue: "",
         sentences: ""
     )
+    
+    @Published var isWrong: Bool = false
+    @Published var isCorrect: Bool = false
 
     func wrongAnswer() {
-        
+        self.isWrong = true
     }
+    
     
     func changeLanguage(language: TraditionalLanguage) {
         self.traditionalLanguage = language
@@ -29,15 +33,22 @@ class TraditionalLanguageController : ObservableObject {
     
     func guessWord(word : String, remainingTime: Int) {
         if (traditionalLanguage.answer == word.uppercased()) {
+            print("BENERR")
             correctAnswer(remainingTime: remainingTime)
         }
         else {
+            print("HAA")
             wrongAnswer()
+        }
+        print(word)
+        print(traditionalLanguage.answer)
+        if word.uppercased() == traditionalLanguage.answer {
+            print("WTF")
         }
     }
     
     func correctAnswer(remainingTime: Int) {
-        
+        self.isCorrect = true
     }
     
     func accumulatePoint() {
